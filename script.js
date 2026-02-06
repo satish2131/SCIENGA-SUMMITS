be39b1856a1b697555849e251206df034ac013ed
satish2131 <satishcm035@gmail.com>
2026-02-06 17:11:35 +0530

    uploading

---
+++ script.js
@@ -0,0 +1,18 @@
+// Register button action
+document.getElementById("registerBtn").addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Registration will open soon. Stay tuned!");
+});
+
+// Abstract submission button action
+document.getElementById("abstractBtn").addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Abstract submission portal will be available soon.");
+});
+
+sendBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Thank you for contacting us! We will get back to you shortly.!");});
+
+
+    
